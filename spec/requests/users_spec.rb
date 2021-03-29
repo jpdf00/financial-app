c26676ac7cd6036bc15078053968a6e69c9db1a3
jpdf00 <j.pdf00@gmail.com>
2021-03-29 09:33:14 -0300

    Change tests, update style
    Change tests to use Capybara.
    Update the style for Index, show and form for groups and payments

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,54 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Siggin Up', type: :feature do
+  scenario 'Sign up with valid inputs' do
+    visit new_user_registration_path
+    fill_in 'user[name]', with: 'Foo Bar'
+    fill_in 'user[email]', with: 'foo@bar.com'
+    fill_in 'user[password]', with: '12345678'
+    fill_in 'user[password_confirmation]', with: '12345678'
+    click_on 'Create Account'
+    expect(page).to have_content('Foo Bar')
+  end
+
+  scenario 'Sign up with invalid inputs' do
+    visit new_user_registration_path
+    fill_in 'user[name]', with: ''
+    fill_in 'user[email]', with: 'foo@bar.com'
+    fill_in 'user[password]', with: '12345678'
+    fill_in 'user[password_confirmation]', with: '12345678'
+    click_on 'Create Account'
+    expect(page).to have_content("Name can't be blank")
+  end
+end
+
+RSpec.describe 'Sign In', type: :feature do
+  let(:user) { User.create(name: 'Foo Bar', email: 'foo@bar.com', password: '12345678') }
+  scenario 'Log in with valid inputs' do
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_on 'Log In'
+    expect(page).to have_content('Foo Bar')
+  end
+
+  scenario 'Log in with invalid inputs' do
+    visit new_user_session_path
+    fill_in 'user[email]', with: ''
+    fill_in 'user[password]', with: user.password
+    click_on 'Log In'
+    expect(page).to have_content('Invalid Email or password.')
+  end
+end
+
+RSpec.describe 'Loggin Out', type: :feature do
+  let(:user) { User.create(name: 'Foo Bar', email: 'foo@bar.com', password: '12345678') }
+  scenario 'Log Out' do
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_on 'Log In'
+    click_on 'Log Out'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
   end
 end
